565d74fce9846fc86f1ba035ee92447670222947
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-02 19:52:30 -0500

    Codigo NRZ-M

--- P2Codigos/Codigos.m
+++ P2Codigos/Codigos.m
@@ -20,6 +20,7 @@ plot(t, nrzl);
 title('Codigo');
 axis([0, N -1.5 1.5]);
 grid on;
+
 % Espectro NRZL
 N=300;
 nrzl=[];
@@ -39,12 +40,14 @@ grid on;
 %% NRZ-M
 N=10;
 s1=ones(1,100);
-bits=randint(1,N)*2-1;
-marca=bits(1);
+bits=randint(1,N)*2-1
 nrzm=[];
+marca=bits(1);
 for n=1:N
-    
-    nrzm=[nrzm s1*bits(n)];
+    if(bits(n)==1)
+        marca=-1*marca;
+    end
+    nrzm=[nrzm s1*marca];
 end
 
 figure(2);
@@ -54,26 +57,28 @@ title('Codigo');
 axis([0, N -1.5 1.5]);
 grid on;
 
-% Espectro NRZM
+% Espectro NRZ-M
 N=300;
-nrzm=[1];
+nrzm=[];
 bits=randint(1,N)*2-1; %Quitamos corriente directa modificando los niveles
+marca=bits(1);
 for n=1:N
-    if((n-1)==0)
-        nrzm=[nrzm s1*bits(n)];
-    else
-        nrzm=[nrzm s1*bits(n)];
+    if(bits(n)==1)
+        marca=-1*marca;
     end
+    nrzm=[nrzm s1*marca];
 end
 NRZM=fftshift(fft(nrzm,100000))*ts;
 f=linspace(-1/(2*ts),1/(2*ts),length(NRZM));
-
 figure(2);
 subplot(2, 1, 2);
 plot(f, abs(NRZM));
 title('Espectro');
 axis([-5 5 -0.5 max(abs(NRZM))]);
 grid on;
+
+%% NRZ-S
+
 
 %% Machester
 %s0=[zeros(1,50) ones(1,50)]
