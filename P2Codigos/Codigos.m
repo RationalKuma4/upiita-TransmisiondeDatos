a99ee16d7fce5c325b18bc69444ea4a7d774d065
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-04 21:54:01 -0500

    Codigos completos
    Porblamente haya que revisar algunos, pero tal vez no

--- P2Codigos/Codigos.m
+++ P2Codigos/Codigos.m
@@ -1,3 +1,4 @@
+clc;
 clear all;
 close all;
 
@@ -19,6 +20,7 @@ subplot(2, 1, 1);
 plot(t, nrzl);
 title('Codigo NRZ-L');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro NRZL
@@ -53,6 +55,7 @@ subplot(2, 1, 1);
 plot(t, nrzm);
 title('Codigo NRZ-M');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro NRZ-M
@@ -91,6 +94,7 @@ subplot(2, 1, 1);
 plot(t, nrzs);
 title('Codigo NRZ-S');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro NRZ-S
@@ -131,6 +135,7 @@ subplot(2, 1, 1);
 plot(t, rz);
 title('Codigo RZ');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro RZ
@@ -185,6 +190,7 @@ subplot(2, 1, 1);
 plot(t, bp);
 title('Codigo Bipolar');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro Bipolar
@@ -235,6 +241,7 @@ subplot(2, 1, 1);
 plot(t, manchester);
 title('Codigo Manchester');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
 % Espectro Manchester
@@ -273,9 +280,10 @@ subplot(2, 1, 1);
 plot(t, bfm);
 title('Codigo Bifase Marca');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
 grid on;
 
-Espectro Bifase Marca
+%Espectro Bifase Marca
 N=300;
 bits=randint(1,N)*2-1;
 bfm=[];
@@ -299,25 +307,188 @@ grid on;
 
 %% Bifase Espacio
 N=10;
-s1=[ones(1,50) -1*ones(1,50)];
+s1=[-1*ones(1,50) ones(1,50)];
 s2=ones(1,100);
+%bits=[1 -1 -1 1 -1 -1 1 1 1 -1 -1 -1 1];
 bits=randint(1,N)*2-1;
-bfm=[];
+bfs=[];
 marca=bits(1);
 for n=1:N
     marca=-1*marca;
     if(bits(n)==-1)
-        bfm=[bfm s1*bits(n)*marca];
+        bfs=[bfs s1*bits(n)*marca];
         marca=-1*marca;
     else
-        bfm=[bfm s2*marca];
+        bfs=[bfs s2*marca];
     end
 end
-figure(7);
+figure(8);
 subplot(2, 1, 1);
-plot(t, bfm);
-title('Codigo Bifase Marca');
+plot(t, bfs);
+title('Codigo Bifase Espacio');
 axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
+grid on;
+
+%Espectro Bifase Espacio
+N=300;
+bits=randint(1,N)*2-1;
+bfs=[];
+marca=bits(1);
+for n=1:N
+    marca=-1*marca;
+    if(bits(n)==-1)
+        bfs=[bfs s1*bits(n)*marca];
+        marca=-1*marca;
+    else
+        bfs=[bfs s2*marca];
+    end
+end
+BFS=fftshift(fft(bfs,100000))*ts;
+f=linspace(-1/(2*ts),1/(2*ts),length(BFS));
+subplot(2, 1, 2);
+plot(f, abs(BFS));
+title('Espectro');
+axis([-5 5 -0.5 max(abs(BFS))]);
+grid on;
+
+%% Manchester Diferencial *
+N=10;
+s1=[zeros(1,50) ones(1,50)];
+s0=[ones(1,50) zeros(1,50)];
+%bits=[1 -1 -1 1 -1 -1 1 1 1 -1 -1 -1 1];
+bits=randint(1,N)*2-1;
+marca=bits(1);
+md=[];
+for n=1:N
+    marca=-1*marca;
+    if(marca==-1)
+        if(bits(n)==1)
+            md=[md s0];
+        else
+            md=[md s1];
+        end
+    else
+        if(bits(n)==1)
+            md=[md s1];
+        else
+            md=[md s0];
+        end
+        marca=-1*marca;
+    end
+end
+figure(9);
+subplot(2, 1, 1);
+plot(t, md);
+title('Codigo Manchester Diferencial');
+axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
+grid on;
+
+%Manchester Diferencial
+N=300;
+bits=randint(1,N)*2-1;
+marca=bits(1);
+md=[];
+for n=1:N
+    marca=-1*marca;
+    if(marca==-1)
+        if(bits(n)==1)
+            md=[md s0];
+        else
+            md=[md s1];
+        end
+    else
+        if(bits(n)==1)
+            md=[md s1];
+        else
+            md=[md s0];
+        end
+        marca=-1*marca;
+    end
+end
+MD=fftshift(fft(md,100000))*ts;
+f=linspace(-1/(2*ts),1/(2*ts),length(MD));
+subplot(2, 1, 2);
+plot(f, abs(MD));
+title('Espectro');
+axis([-5 5 -0.5 max(abs(MD))]);
+grid on;
+
+%% Miller *verificar
+N=10;
+s1=[ones(1,50) -1*ones(1,50)];
+s0=ones(1,100);
+bits=randint(1,N)*2-1;
+mi=[];
+
+if(bits(1)==1)
+    mi=[mi s1*bits(1)];
+else
+    mi=[mi s0];
+end
+for n=2:N
+    if(bits(n)==1)
+        if(bits(n-1)==1)
+            if(mi((n-1)*100-1)==-1)
+                 mi=[mi -1*s1];
+            else
+                mi=[mi s1];
+            end
+        else
+            if(mi((n-1)*100-1)==-1)
+                 mi=[mi 1*s1];
+            else
+                mi=[mi -1*s1];
+            end
+        end
+        
+    else
+        mi=[mi s0*bits(n)];
+    end
+end
+figure(10);
+subplot(2, 1, 1);
+plot(t, mi);
+title('Codigo Miller');
+axis([0, N -1.5 1.5]);
+set(gca,'xticklabel',bits.')
+grid on;
+
+%Miller Diferencial
+N=300;
+bits=randint(1,N)*2-1;
+if(bits(1)==1)
+    mi=[mi s1*bits(1)];
+else
+    mi=[mi s0];
+end
+for n=2:N
+    if(bits(n)==1)
+        if(bits(n-1)==1)
+            if(mi((n-1)*100-1)==-1)
+                 mi=[mi -1*s1];
+            else
+                mi=[mi s1];
+            end
+        else
+            if(mi((n-1)*100-1)==-1)
+                 mi=[mi 1*s1];
+            else
+                mi=[mi -1*s1];
+            end
+        end
+        
+    else
+        mi=[mi s0*bits(n)];
+    end
+end
+MI=fftshift(fft(mi,100000))*ts;
+f=linspace(-1/(2*ts),1/(2*ts),length(MI));
+subplot(2, 1, 2);
+plot(f, abs(MI));
+title('Espectro');
+axis([-5 5 -0.5 max(abs(MI))]);
 grid on;
 
 %% Machester
